f6001356c5aa595505141d4ea619607ba22e5570
Spas Milenkov <spasmilenkov123@gmail.com>
2024-02-17 22:03:38 +0200

    Setup Tailwind, Vue prime and the Lara preset

---
+++ UniTrackFrontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import SystemComponent from './components/SystemComponent.vue';
+</script>
+
+<template>
+  <div class="dark:bg-gray-900 min-h-screen min-w-screen flex items-center justify-center gap-5">
+    <SystemComponent />
+
+  </div>
+</template>
+
+<style scoped></style>
