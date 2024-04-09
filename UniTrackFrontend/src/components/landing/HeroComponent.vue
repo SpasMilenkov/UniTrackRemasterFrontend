6f4732b74d24354c5ab668991a84f896bef7ce32
Spas Milenkov <spasmilenkov123@gmail.com>
2024-04-09 21:49:19 +0300

    Add hero component skeleton

--- UniTrackFrontend/src/components/landing/HeroComponent.vue
+++ UniTrackFrontend/src/components/landing/HeroComponent.vue
@@ -1,5 +1,17 @@
 <template>
-    <div></div>
+    <div class=" flex items-center justify-between bg-surface:100 dark:bg-surface-950 w-full min-h-[31rem]">
+        <div class="flex flex-col content-between gap-20 w-2/3 p-24 h-full">
+            <h1 class=" text-surface-950 dark:text-surface-200 text-8xl">UniTrack</h1>
+            <div class="flex content-between gap-16">
+                <Button label="Log in"></Button>
+                <Button outlined label="Documentation"></Button>
+            </div>
+        </div>
+        <div class="flex w-1/3 bg-surface-100 dark:bg-surface-900 self-stretch">aaaaa</div>
+    </div>
 </template>
-<script setup lang='ts'></script>
-<style scoped></style>
+<script setup lang='ts'>
+import Button from 'primevue/button';
+</script>
+<style scoped>
+</style>
--- UniTrackFrontend/src/views/HomeView.vue
+++ UniTrackFrontend/src/views/HomeView.vue
@@ -1,3 +1,7 @@
-<template></template>
-<script setup lang='ts'></script>
+<template>
+    <HeroComponent></HeroComponent>
+</template>
+<script setup lang='ts'>
+import HeroComponent from '../components/landing/HeroComponent.vue';
+</script>
 <style scoped></style>
