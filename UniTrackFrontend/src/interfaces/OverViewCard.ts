25db90a7199b0eb8ae33262c7d897d8fc95cbe87
Spas Milenkov <spasmilenkov123@gmail.com>
2024-04-11 23:08:26 +0300

    Add interfaces for the new components

---
+++ UniTrackFrontend/src/interfaces/OverViewCard.ts
@@ -0,0 +1,12 @@
+export default interface OverviewCard {
+    value: number,
+    color: string,
+    cardTitle: string,
+    cardDataMain: string,
+    cardDataSecondary: string,
+    cardDataTertiary: string,
+    knobMinValue: number,
+    knobMaxValue: number,
+    knobStrokeColor: string,
+    knobEmptyColor: string,
+}
