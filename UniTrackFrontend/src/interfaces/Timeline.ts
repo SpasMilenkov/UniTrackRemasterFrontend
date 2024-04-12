ab98822fab4f66ee8f99f50f7bef53a1d08edc97
Spas Milenkov <spasmilenkov123@gmail.com>
2024-04-12 12:04:23 +0300

    Add timeline interface

---
+++ UniTrackFrontend/src/interfaces/Timeline.ts
@@ -0,0 +1,12 @@
+export interface Timeline {
+    title: string,
+    subtitle: string,
+    content: string,
+}
+
+export interface MarkTimeline {
+    title: string
+    subtitle: Date
+    content: string
+    markValue: number
+}
