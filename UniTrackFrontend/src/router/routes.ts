9ac7aab579d071c6f69d8628230878c56d731c0c
Spas Milenkov <spasmilenkov123@gmail.com>
2024-04-13 00:40:24 +0300

    Add teacher dashboard to the router

--- UniTrackFrontend/src/router/routes.ts
+++ UniTrackFrontend/src/router/routes.ts
@@ -2,6 +2,7 @@ import HomeView from '../views/landing/HomeView.vue'
 import LoginView from '../views/auth/LoginView.vue'
 import RegisterView from '../views/auth/RegisterView.vue'
 import StudentDashboardView from '../views/student/StudentDashboardView.vue'
+import TeacherDashboardView from '../views/teacher/TeacherDashboardView.vue'
 const routes = [
     {
         path: '/',
@@ -23,6 +24,11 @@ const routes = [
         name: 'studentDashboard',
         component: StudentDashboardView
     },
+    {
+        path: '/teacher',
+        name: 'teacherDashboard',
+        component: TeacherDashboardView
+    }
 ]
 
 export default routes
--- UniTrackFrontend/src/views/teacher/TeacherDashboardView.vue
+++ UniTrackFrontend/src/views/teacher/TeacherDashboardView.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="flex flex-col p-8 w-full dark:bg-surface-950 min-h-[100vh] gap-4">
-        <ProfileCard></ProfileCard>
+        <div class="flex justify-between gap-8 items-center flex-wrap">
+            <ProfileCard></ProfileCard>
+            <EventCard></EventCard>
+        </div>
         <MarkGrid></MarkGrid>
     </div>
 
@@ -8,5 +11,6 @@
 <script setup lang='ts'>
 import MarkGrid from '../../components/teacher/StudentGridComponent.vue';
 import ProfileCard from '../../components/ProfileCardComponent.vue';
+import EventCard from '../../components/teacher/EventCardComponent.vue'
 </script>
 <style scoped></style>
