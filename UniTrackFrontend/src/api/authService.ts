e56b7a17b300862eec06c98557889a60a10cfebc
Spas Milenkov <spasmilenkov123@gmail.com>
2024-04-14 16:36:52 +0300

    Add auth service

---
+++ UniTrackFrontend/src/api/authService.ts
@@ -0,0 +1,19 @@
+import LoginDto from "../interfaces/requests/LoginDto";
+import { RegisterDto } from "../interfaces/requests/RegisterDto";
+import axiosInstance from "./axiosInstance";
+
+const login = async (loginDto: LoginDto) => {
+    const result = await axiosInstance.post('Auth/login', loginDto)
+    return result
+}
+
+const register = async (registerDto: RegisterDto) => {
+    const result = await axiosInstance.post('Auth/register', registerDto)
+    return result
+}
+
+const logout = async () => {
+    const result = await axiosInstance.post('Auth/logout')
+    return result
+}
+export {login, register}
