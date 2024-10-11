8e2732e5d1c1c096cc6dcb2c7f1e0bd550a18b89
Spas Milenkov <spasmilenkov123@gmail.com>
2024-10-11 21:48:46 +0300

    feat: add naive config

---
+++ UniTrackRemasterFrontend/utils/naive-ui-forms.config.ts
@@ -0,0 +1,12 @@
+export const naiveUiFormsConfig = (state: { errors: unknown[]; }) =>
+  ({
+    props: {
+      validationStatus: state.errors[0] ? 'error' : undefined,
+      feedback: state.errors[0],
+    },
+  } as {
+    props: {
+      validationStatus: 'error' | 'warning' | 'success' | undefined;
+      feedback: string | undefined;
+    };
+  });
