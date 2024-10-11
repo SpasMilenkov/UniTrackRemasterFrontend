0b36c66ce480147b433f6fca94013387d69efa03
Spas Milenkov <spasmilenkov123@gmail.com>
2024-10-11 21:48:56 +0300

    feat: add password check util

---
+++ UniTrackRemasterFrontend/utils/password.util.ts
@@ -0,0 +1,6 @@
+const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+export const validatePassword = (password: string) => {
+  return passwordRegex.test(password);
+};
