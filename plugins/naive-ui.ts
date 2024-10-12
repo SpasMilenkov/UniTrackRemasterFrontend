2c91fd557f5bbff823f01f83267757245371ba25
Spas Milenkov <spasmilenkov123@gmail.com>
2024-10-12 23:06:26 +0300

    feat: add naive plugin to fix css loading issues

---
+++ plugins/naive-ui.ts
@@ -0,0 +1,57 @@
+// A somewhat hacky solution to fix the delay in CSS load when using naive.
+// Apparently this issue was caused in a not so recent update to Nuxt...
+// Without this plugin css loads considerable slower when refreshing the page
+// This behavior is witnessed in both dev and preview modes
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { setup } from '@css-render/vue3-ssr';
+export default defineNuxtPlugin((nuxtApp) => {
+  if (import.meta.server) {
+    const { collect } = setup(nuxtApp.vueApp);
+    const originalRenderMeta: any = nuxtApp.ssrContext?.renderMeta;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    nuxtApp.ssrContext = nuxtApp.ssrContext || {};
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    nuxtApp.ssrContext.renderMeta = () => {
+      if (!originalRenderMeta) {
+        return {
+          headTags: collect(),
+        };
+      }
+      const originalMeta = originalRenderMeta();
+      if ('then' in originalMeta) {
+        return originalMeta.then((resolvedOriginalMeta: any) => {
+          return {
+            ...resolvedOriginalMeta,
+            headTags: resolvedOriginalMeta.headTags + collect(),
+          };
+        });
+      } else {
+        return {
+          ...originalMeta,
+          headTags: originalMeta.headTags + collect(),
+        };
+      }
+    };
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    nuxtApp.ssrContext.head = nuxtApp.ssrContext.head || [];
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    nuxtApp.ssrContext.head.push({
+      style: () =>
+        collect()
+          .split('</style>')
+          .map((block) => {
+            const id = RegExp(/cssr-id="(.+?)"/).exec(block)?.[1];
+            const style = (RegExp(/>(.*)/s).exec(block)?.[1] ?? '').trim();
+            return {
+              'cssr-id': id,
+              innerHTML: style,
+            };
+          }),
+    });
+  }
+});
