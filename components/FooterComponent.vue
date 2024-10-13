f09b654898ac36aa480c835d108c7f5775d5a700
Spas Milenkov <spasmilenkov123@gmail.com>
2024-10-13 23:16:42 +0300

    feat: add placeholder footer component

---
+++ components/FooterComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <n-layout-footer bordered>
+        <n-space justify="center" align="center" style="height: 100%">
+            <n-text>&copy; 2024 UniTrack. All rights reserved.</n-text>
+        </n-space>
+    </n-layout-footer>
+</template>
+
+<script setup>
+import { NLayoutFooter, NSpace, NText } from 'naive-ui'
+</script>
+
+<style scoped>
+.n-layout-footer {
+    height: 60px;
+}
+</style>
