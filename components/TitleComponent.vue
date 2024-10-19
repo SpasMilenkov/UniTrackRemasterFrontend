f6d5646bc66eeca19f483bcffa53d870ee4d8983
Spas Milenkov <spasmilenkov123@gmail.com>
2024-10-19 22:52:18 +0300

    feat: add title component

---
+++ components/TitleComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1 class="md:px-0 text-center lg:text-left" :class="{
+        'text-3xl': size === 'large',
+        'text-2xl': size === 'medium',
+        'text-xl': size === 'small'
+    }">{{ title }}</h1>
+</template>
+<script setup lang='ts'>
+defineProps(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        size: {
+            type: String as () => 'large' | 'medium' | 'small',
+            default: () => 'large'
+        }
+    }
+)
+</script>
+<style scoped></style>
