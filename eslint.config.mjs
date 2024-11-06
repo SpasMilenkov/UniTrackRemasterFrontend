e511d7f85193eb257c2ba9ce5b59b92748205767
Spas Milenkov <spasmilenkov123@gmail.com>
2024-11-06 03:23:10 +0200

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,5 +4,6 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 export default withNuxt({
   rules: {
     'vue/first-attribute-linebreak': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 });
