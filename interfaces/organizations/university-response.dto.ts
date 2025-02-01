8dce64eb93f9726e2a9716c83f623802a3066b77
Spas Milenkov <spasmilenkov123@gmail.com>
2025-02-01 22:27:11 +0200

    feat: add university response dto

---
+++ interfaces/organizations/university-response.dto.ts
@@ -0,0 +1,14 @@
+export interface UniversityResponse {
+  id: string;
+  name: string;
+  motto: string;
+  description: string;
+  email: string;
+  phone: string;
+  website: string;
+  establishedDate: string;
+  accreditations: number[];
+  programs: string[];
+  fileUrls: string[];
+  message: string;
+}
