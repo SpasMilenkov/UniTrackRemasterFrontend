ff4bed33da91a4ba3c6d116550ea4c53bdfada07
Spas Milenkov <spasmilenkov123@gmail.com>
2025-02-01 22:20:07 +0200

    feat: add logo component

---
+++ components/LogoComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex gap-2 items-center justify-center p-2">
+    <img src="/img/logo.png" alt="UniTrack Logo" class="h-10 w-auto" />
+    <span
+      class="text-xl font-bold bg-gradient-to-r from-emerald-400 to-blue-500 text-transparent bg-clip-text"
+    >
+      UniTrack
+    </span>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped></style>
