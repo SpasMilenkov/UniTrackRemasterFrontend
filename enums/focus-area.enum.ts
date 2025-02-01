cd2c2e14676b67dd406f40ecc74c6c0bc576bcf6
Spas Milenkov <spasmilenkov123@gmail.com>
2025-02-01 22:23:08 +0200

    feat: add focus area enum

---
+++ enums/focus-area.enum.ts
@@ -0,0 +1,11 @@
+
+export enum FocusArea {
+    LiberalArts = "Liberal Arts",
+    STEM = "STEM",
+    Business = "Business",
+    Medical = "Medical",
+    Law = "Law",
+    Arts = "Arts",
+    Technical = "Technical",
+    Research = "Research"
+}
