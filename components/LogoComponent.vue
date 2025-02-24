bc85dce472236fa6c47788126e60988343d0c32f
Spas Milenkov <spasmilenkov123@gmail.com>
2025-02-24 16:11:17 +0200

    refactor: change img tag to NuxtImage

--- components/LogoComponent.vue
+++ components/LogoComponent.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="flex gap-2 items-center justify-center p-2">
-    <img src="/img/logo.png" alt="UniTrack Logo" class="h-10 w-auto" />
+    <NuxtPicture
+      src="/images/logo.png"
+      alt="UniTrack Logo"
+      class="h-10 w-10"
+    />
     <span
       class="text-xl font-bold bg-gradient-to-r from-emerald-400 to-blue-500 text-transparent bg-clip-text"
     >
