c08a19ccf3003fe0cbd96be3f9a0f081071fb6db
Spas Milenkov <spasmilenkov123@gmail.com>
2025-06-23 21:11:43 +0300

    feat: add filter parameter interfaces for absence breakdown, at-risk students, attendance, marks, statistics, and teacher search

---
+++ interfaces/teacher/filters/mark-filter-params.ts
@@ -0,0 +1,8 @@
+
+export interface MarkFilterParams {
+  semesterId?: string;
+  subjectId?: string;
+  studentId?: string;
+  fromDate?: string;
+  toDate?: string;
+}
---
+++ interfaces/teacher/filters/teacher-search-params.ts
@@ -0,0 +1,10 @@
+export interface TeacherSearchParams {
+  query?: string;
+  institutionId?: string;
+  departmentId?: string;
+  classGradeId?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: string;
+  ascending?: boolean;
+}
